8af61abe00f8aa3b30cb1cdb565fe32eb921907b
Codeo-Oguzhan <133108670+Codeo-Oguzhan@users.noreply.github.com>
2025-11-09 15:26:38 +0300

    create tests project

---
+++ VastlySimTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace VastlySimTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
